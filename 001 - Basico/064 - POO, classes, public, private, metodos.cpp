322246b1acb912219692a26479bf0cb9931e1b98
philipheferreira <philipheferreira@gmail.com>
2026-06-11 17:59:31 -0300

    Feat: create basic projects

---
+++ 001 - Basico/064 - POO, classes, public, private, metodos.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+
+class Aviao {
+    
+	public:
+		int vel;
+    
+};
+
+int main() {
+    
+   Aviao *av1 = new Aviao();
+   
+   cout << av1 -> vel;
+    
+    
+}
