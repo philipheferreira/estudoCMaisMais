fa7b56eecffd7d4a7bdfcf3444e595498ffaff21
philipheferreira <philipheferreira@gmail.com>
2026-06-28 18:34:37 -0300

    Feat: finish POO in C++

--- 001 - Basico/075 - POO, Heranca, virtual, override.cpp
+++ 001 - Basico/075 - POO, Heranca, virtual, override.cpp
@@ -2,50 +2,122 @@
 #include <stdlib.h>
 
 
-/*  */
+/* Heranca: Basicamente quando eu crio uma classe que herda caracteristicas
+de outra classe  */
 
+/* Virtual: O virtual sera utilizado junto com o override. O objetivo que 
+o virtual vai ser definido na classe que vai passar sua heranca para a outra
+classe. dentro de um metodo na classe pai eu vou definir o virtual na frente
+do metodo para realizar as operacoes. Assim sinalizando que essa classe, caso
+exista essa mesma em uma classe que esta recebendo essa heranca, ela sera 
+substituida por essa outra classe com o mesmo nome na classe que estiver 
+recebendo a heranca */
+
+/* Override: O Override sera utilizado junto com o virtual. O override sera
+definido na classe que receber a heranca. Caso exista um metodo do mesmo nome
+nessa classe, o mesmo metodo existe na classe de origem, so que eu quero utilizar
+o metodo da classe que recebe a heranca e nao da de origem. O override significa
+que vou utilizar a classe que esta com o mesmo metodo definido na classe que 
+recebe a heranca. Ou seja, o metodo que tem o mesmo nome do metodo dentro da 
+origem sera sobreescrito pelo metodo da classe que recebe a heranca */
 
 using namespace std;
 
-class Carro{
+class Veiculo{
 	private:
-		int velMax;
 		const char* nome;
+		const char* cor;
     public:
-    	int potencia;
-    	int getVelMax(){
-			return velMax;
+    	int velMax;
+    	int rodas;
+    	
+    	void setNome(const char* no){
+			nome = no;
 		}
+		
 		const char* getNome(){
 			return nome;
 		}
-		Carro(){
-			velMax = 120;
-			potencia = 85;
-			nome = "Popular";
+		
+		void setCor(const char* co){
+			cor = co;
 		}
-		Carro(int pt, const char* no):potencia(pt), nome(no){ /*  */
-			if(pt < 100){
-				velMax = 120;
-			}else 
-			if(pt < 200){
-				velMax = 240;
+		
+		const char* getCor(){
+			return cor;
+		}
+		
+		virtual void imp(){ /* Quando tiver o virtual, significa que esse metodo
+		sera sobescrito em alguma classe que herdar essa classe veiculo */
+			cout << "Nome.................: " << nome << endl;
+			cout << "Cor..................: " << cor << endl;
+			cout << "Rodas................: " << rodas << endl;
+			cout << "Vel.Max..............: " << velMax << endl;
+		}
+};
+
+class Carro:public Veiculo{
+	public:
+		Carro(){
+			velMax = 160;
+			rodas = 4;
+			setNome("Carro");
+			setCor("Branco");
+		}
+};
+
+class Moto:public Veiculo{
+	public:
+		Moto(){
+			velMax = 200;
+			rodas = 2;
+			setNome("Moto");
+			setCor("Preto");
+		}
+};
+
+class Militar:public Veiculo{
+	public:
+		int monicao;
+		bool armamento;
+		Militar(bool arma, int mo):armamento(arma), monicao(mo){
+			velMax = 150;
+			rodas = 6;
+			setNome("Tanque");
+			setCor("Verde");
+			
+			if(arma){
+				monicao = mo;
 			}else{
-				velMax = 360;
+				monicao = 0;
 			}
+		}
+		/* Caso eu tente acessar variaveis privadas eu nao irei conseguir */
+		void imp(){ /* aqui vai ter que ficar o override assim:  void imp() override{
+		O problema que como estou usando um compilador antigo ele nao entende o 
+		funcionamento de algo mais atual assim*/
+			cout << "Nome.................: " << getNome() << endl;
+			cout << "Cor..................: " << getCor() << endl;
+			cout << "Rodas................: " << rodas << endl;
+			cout << "Vel.Max..............: " << velMax << endl;
+			cout << "Armamento............: " << armamento << endl;
+			cout << "Monicao..............: " << monicao << endl;
 		}
 };
 
 int main() {
 	
-	Carro c1;
-	cout << c1.getNome() << " - " << c1.potencia << " - " << c1.getVelMax() << endl;
+	Carro v1;
+	v1.imp();
 	
-	Carro c2(180, "Luxo");
-	cout << c2.getNome() << " - " << c2.potencia << " - " << c2.getVelMax()  << endl;
+	Moto v2;
+	v2.imp();
 	
-	Carro c3(300, "Esportivo");
-	cout << c3.getNome() << " - " << c3.potencia << " - " << c3.getVelMax() << endl;
+	Militar v3(false, 200);
+	v3.imp();
+	
+	Militar v4(true, 100);
+	v4.imp();
 	
 	system("pause");
 	
--- 001 - Basico/076 - POO, Protected x Private x Public.cpp
+++ 001 - Basico/076 - POO, Protected x Private x Public.cpp
@@ -2,51 +2,95 @@
 #include <stdlib.h>
 
 
-/*  */
+/* 
+
+private:
+So pode ser acessado dentro da propria classe. Se eu tentar acessar o private
+fora da classe original sera negado
+
+
+protected: Parece com o private no quesito bloquear acesso, entretanto ele 
+permite acesso para classes que herdarem as informacoes da classe de origem,
+permitindo o uso das informacoes dentro da classe de origem e qualquer uma
+das que herdaram ela. Proibe acesso de qualquer uma que nao seja a classe de 
+origem ou as herdadas dela
+
+public:
+Tudo que for public pode ser acessado por dentro da classe ou fora da classe.
+Por membros herdados ou por quando essa classe for utilizada
+
+
+ */
 
 
 using namespace std;
 
-class Carro{
+class Veiculo{
 	private:
 		int velMax;
-		const char* nome;
+		int potencia;
     public:
-    	int potencia;
-    	int getVelMax(){
-			return velMax;
-		}
-		const char* getNome(){
-			return nome;
-		}
-		Carro(){
-			velMax = 120;
-			potencia = 85;
-			nome = "Popular";
-		}
-		Carro(int pt, const char* no):potencia(pt), nome(no){ /*  */
-			if(pt < 100){
-				velMax = 120;
-			}else 
-			if(pt < 200){
-				velMax = 240;
-			}else{
-				velMax = 360;
-			}
+    	int rodas;
+    	const char* nome;
+ 	protected:
+ 		int portas;
+ 		const char* cor;
+};
+
+class Carro:public Veiculo{
+public:	
+	Carro(){
+		//velMax = 180; 
+		/* velMax uma variavel privada, logo nao vai funcionar o acesso a essa
+		variavel*/
+		//potencia = 150;
+		/* potencia uma variavel privada, logo nao vai funcionar o acesso a essa
+		variavel */
+		rodas = 4;
+		nome = "Carro";
+		portas = 4;
+		cor = "Vermelho";
+		
+		//cout << velMax << endl; 
+		/* Para o codigo rodar preciso utilizar velMax comentanda pois ser uma 
+		variavel privada, logo vai dar erro */
+		//cout << potencia << endl;
+		/* Para o codigo rodar preciso utilizar potencia comentanda pois ser uma 
+		variavel privada, logo vai dar erro */
+		cout << rodas << endl;
+		cout << nome << endl;
+		cout << portas << endl;
+		cout << cor << endl;
+		
+	}
+	
+};
+
+class Moto{
+	public:
+		Carro c;
+		Moto(){
+			//c.velMax = 250;
+			//c.potencia = 500;
+			c.rodas = 2;
+			c.nome = "Moto";
+			//c.portas = 0;
+			//c.cor = "preto";
+			
+			//cout << c.velMax << endl;
+			//cout << c.potencia << endl;
+			cout << endl << endl << c.rodas << endl;
+			cout << c.nome << endl;
+			//cout << c.portas << endl;
+			//cout << c.cor << endl;
 		}
 };
 
 int main() {
 	
-	Carro c1;
-	cout << c1.getNome() << " - " << c1.potencia << " - " << c1.getVelMax() << endl;
-	
-	Carro c2(180, "Luxo");
-	cout << c2.getNome() << " - " << c2.potencia << " - " << c2.getVelMax()  << endl;
-	
-	Carro c3(300, "Esportivo");
-	cout << c3.getNome() << " - " << c3.potencia << " - " << c3.getVelMax() << endl;
-	
+	Carro v1;
+	cout << endl << endl;
+	Moto v2;
 	system("pause");
 	
 }
